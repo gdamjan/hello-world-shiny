0943b599632d4bb2184cfe1a0c9207a3c9e8d4a2
novica <nnovica@gmail.com>
2021-03-06 20:56:51 +0100

    Update app.r
    Add another function to check if the way we run the app for the docker image will work.

--- src/app.r
+++ src/app.r
@@ -1,30 +1,43 @@
 library(shiny)
+library(ggplot2)
+
 
 ui <- bootstrapPage(
   h3("URL components"),
   verbatimTextOutput("urlText"),
-
+  
   h3("Parsed query string"),
-  verbatimTextOutput("queryText")
+  verbatimTextOutput("queryText"),
+  
+  
+  plotOutput("ggPlot")
+  
 )
 
 server <- function(input, output, session) {
-
+  
   # Return the components of the URL in a string:
   output$urlText <- renderText({
     paste(sep = "",
-      "protocol: ", session$clientData$url_protocol, "\n",
-      "hostname: ", session$clientData$url_hostname, "\n",
-      "pathname: ", session$clientData$url_pathname, "\n",
-      "port: ",     session$clientData$url_port,     "\n",
-      "search: ",   session$clientData$url_search,   "\n"
+          "protocol: ", session$clientData$url_protocol, "\n",
+          "hostname: ", session$clientData$url_hostname, "\n",
+          "pathname: ", session$clientData$url_pathname, "\n",
+          "port: ",     session$clientData$url_port,     "\n",
+          "search: ",   session$clientData$url_search,   "\n"
     )
   })
-
+  
+  output$ggPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    ggplot(mtcars) +
+       aes(x = mpg, y = cyl) +
+       geom_point()
+  })
+  
   # Parse the GET query string
   output$queryText <- renderText({
     query <- parseQueryString(session$clientData$url_search)
-
+    
     # Return a string with key-value pairs
     paste(names(query), query, sep = "=", collapse=", ")
   })
